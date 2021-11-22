0e68fc178a43576b615259b649a2d12c9c0b5e71
Fred Zhen <13911239075@163.com>
2021-11-22 11:07:18 +0800

    Rebased and reconfigured the adding table task with correct sequence.

--- src/main/resources/db/migration/V2__add_author.sql
+++ src/main/resources/db/migration/V2__add_author.sql
@@ -1,8 +1,6 @@
-drop table if exists author;
-
 create table author (
-                      id bigint not null,
-                      first_name varchar(255),
-                      last_name varchar(255),
-                      primary key (id)
+                        id bigint not null,
+                        first_name varchar(255),
+                        last_name varchar(255),
+                        primary key (id)
 ) engine=InnoDB;
